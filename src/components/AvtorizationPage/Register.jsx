093d6f1e642dfbb57d6d6d8f264eefe8f2efa15a
Elisey <eliseyslivka@gmail.com>
2024-02-19 21:37:34 +0200

    #6: added file Register and connected whith db.json

--- src/components/AvtorizationPage/Register.jsx
+++ src/components/AvtorizationPage/Register.jsx
@@ -1,32 +1,56 @@
 import React from "react";
+import { useState, useNavigate } from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <div className="">
-                <form className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            <h1>User Registration</h1>
-                        </div>
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <div className="forms-group">
-                                        <label>User Name <span>*</span></label>
-                                        <input></input>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">Register</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-}
 
-export default Register;
+    const [id, idChenge] = useState('');
+    const [name, nameChenge] = useState('');
+    const [password, passwordChenge] = useState('');
+    const [email, emailChenge] = useState('');
+    const [phone, phoneChenge] = useState('');
+    // const navigate=useNavigate();
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        let regobj = { id, name, password, email, phone };
+        // console.log(regobj);
+        fetch("http://localhost:3002/user", {
+            method: "POST",
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(regobj)
+        });
+    }
+        return (
+            <form className="wrapper-register" onSubmit={handlesubmit}>
+                <h1>User Registration</h1>
+                <div className="mb-3">
+                    <label>User Name</label>
+                    <input value={id} onChange={e => idChenge(e.target.value)} className="form-control" placeholder="user" />
+                </div>
+
+                <div className="mb-3">
+                    <label>Password</label>
+                    <input value={password} onChange={e => passwordChenge(e.target.value)} type="password" className="form-control" placeholder="Password" />
+                </div>
+
+                <div className="mb-3">
+                    <label>Full Name</label>
+                    <input value={name} onChange={e => nameChenge(e.target.value)} className="form-control" />
+                </div>
+
+                <div className="mb-3">
+                    <label>Email</label>
+                    <input value={email} onChange={e => emailChenge(e.target.value)} type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="mb-3">
+                    <label>Phone</label>
+                    <input value={phone} onChange={e => phoneChenge(e.target.value)} className="form-control" placeholder="Enter phone number" />
+                </div>
+
+                <button type="submit" className="">Register</button>
+            </form>
+        );
+    };
+
+    export default Register;
