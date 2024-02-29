1fa8f62b1d871a42e13021f1e2e554585a2f34c8
Elisey <eliseyslivka@gmail.com>
2024-02-29 18:35:09 +0200

    #6: Added Context in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Context } from "./Components/AvtorizationPage/Context";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Context>
+            <App />
+        </Context>
+    </React.StrictMode>
 );
 
 
