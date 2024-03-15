92df37d6fec869ca9cde10d28f857b07d7f150ae
Elisey <eliseyslivka@gmail.com>
2024-03-15 14:35:45 +0200

    #6: bug-fix

--- src/components/AvtorizationPage/Context.js
+++ src/components/AvtorizationPage/Context.js
@@ -3,33 +3,27 @@ import { createContext, useEffect, useState } from "react";
 export const CustomContext = createContext();
 
 export const Context = (props) => {
-    const [user, setUser] = useState({
-        email: ''
-    });
-    const [task, setTask] = useState({
-        task: ''
-    })
-
-    useEffect(() => {
-        if (localStorage.getItem('user') !== null) {
-            setUser(JSON.parse(localStorage.getItem('user')))
-        }
-    }, []);
-
-    useEffect(() => {
-        if (localStorage.getItem('task') !== null) {
-            setTask(JSON.parse(localStorage.getItem('task')));
-        }
-    }, []);    
-
-    const value = {
-        user,
-        setUser,
-        task,
-        setTask
+  const [user, setUser] = useState({
+    email: ''
+  });
+  const [task, setTask] = useState({
+    task: ''
+  })
+  useEffect(() => {
+    if (localStorage.getItem('user') !== null) {
+      setUser(JSON.parse(localStorage.getItem('user')))
     }
-
-    return <CustomContext.Provider value={value}>
-        {props.children}
-    </CustomContext.Provider>
+    if (localStorage.getItem('task') !== null) {
+      setTask(JSON.parse(localStorage.getItem('task')));
+    }
+  }, []);
+  const value = {
+    user,
+    setUser,
+    task,
+    setTask
+  }
+  return <CustomContext.Provider value={value}>
+    {props.children}
+  </CustomContext.Provider>
 }
