aed44570c4cd14ce79ccdbda5332d376aebd5df0
Elisey <eliseyslivka@gmail.com>
2024-03-15 14:35:14 +0200

    #6: Added the bootstrap

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Context } from "./Components/AvtorizationPage/Context";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Context>
-            <App />
-        </Context>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Context>
+      <App />
+    </Context>
+  </React.StrictMode>
 );
 
 
